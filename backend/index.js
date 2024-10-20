b005a3f2609bfa7f5803811dd8fd647fab393422
Saidhakshin <Saidhakshin75@gmail.com>
2024-10-20 23:35:39 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Replace '*' with the specific origin you want to allow
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/shit", (req, res) => {
+  res.send("Hello bullshit!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
